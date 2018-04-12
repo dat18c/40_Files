ae736c527907552447db2f39e2f78cc5d20a1df0
JarlTuxen <jart@kea.dk>
2018-04-12 17:00:18 +0200

    Fil-eksempler

---
+++ kode/FileTest.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class FileTest{
+
+   public static void main(String[] args) throws FileNotFoundException {
+      
+      Scanner console = new Scanner(System.in);
+      //System.out.print("Angiv filnavn: ");
+      //String fileName = console.nextLine();
+      //Scanner input = new Scanner(new File(fileName));
+      Scanner input = getInputScanner(console);
+      System.out.println();
+      String name;
+      int count = 0;
+      String newName = "";
+      if (input.hasNext()){
+         name = input.next();
+         System.out.print(name);
+         count++;
+         while (input.hasNext()){
+            name = input.next();
+            //System.out.print(" " + name);
+            newName += name;
+            count++;
+         }
+      }
+      System.out.println(newName);
+      System.out.println("Der er " + count + " ord");
+      
+   }
+   
+   public static Scanner getInputScanner(Scanner console) throws FileNotFoundException {
+   
+      System.out.print("Angiv filnavn: ");
+      File f = new File(console.nextLine());
+      while (!f.canRead()){
+         System.out.println("Fil ikke fundet. Prøv igen.");
+         System.out.print("Angiv filnavn: ");
+         f = new File(console.nextLine());
+      }
+      return new Scanner(f);
+   }
+}
