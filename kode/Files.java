8db938f1b054758120638c0e3bfe9de37931caf0
JarlTuxen <jart@kea.dk>
2018-10-28 11:34:06 +0100

    clean up

--- kode/Files.java
+++ kode/Files.java
@@ -5,21 +5,25 @@ import java.util.Scanner;
 public class Files{
 
    public static void main(String[] Args) throws FileNotFoundException {
+      //lav et filobjekt til at læse fra
       File f = new File("tal.txt");
       System.out.println("Fil kan læses: " + f.canRead());
       System.out.println("Filens fuld sti: " + f.getAbsolutePath());
       System.out.println("Filens navn: " + f.getName());
       System.out.println("Filens længde: " + f.length());
       
-      Scanner input = new Scanner(f);
-      double sum = 0.0;
-      while (input.hasNextDouble()){
-         double tal;
-         tal = input.nextDouble();
-         sum = sum + tal;
-         System.out.println(tal); 
-      }
-      System.out.println(sum);
+      //brug fil-objektet som input-strøm
+      
+      //initialiser sum
+
+      //læs inputstrømmen så længe der er flere doubles
+      
+         //læs næste double
+         
+         //læg det læste tal til sum
+         
+      //udskriv sum
+      
    }
 
 }
