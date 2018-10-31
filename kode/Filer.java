25c4946ddf38a72a59598a44eb36595c0fa16892
JarlTuxen <jart@kea.dk>
2018-10-31 13:13:11 +0100

    filer

---
+++ kode/Filer.java
@@ -0,0 +1,28 @@
+//fil pakken
+import java.io.*;
+
+public class Filer
+{
+
+   public static void main(String[] args)
+   {
+      File f = new File("title.basics.tsv");
+      System.out.println("File eksister? " + f.exists());
+      System.out.println("canRead " + f.canRead());
+      System.out.println("Størrelse " + f.length());
+      System.out.println("Path: " + f.getAbsolutePath());
+      
+      //brug fil-objektet som input-strøm
+      
+      //initialiser sum
+
+      //læs inputstrømmen så længe der er flere doubles
+      
+         //læs næste double
+         
+         //læg det læste tal til sum
+         
+      //udskriv sum
+
+   }
+}
